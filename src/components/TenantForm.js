0359414f72b7865f3694c8c542945cd0bb85a300
vinnyWebDev <vinnywebdev@gmail.com>
2023-08-08 21:59:37 +0100

    create working for tenant

--- src/components/TenantForm.js
+++ src/components/TenantForm.js
@@ -1,55 +1,78 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function TenantForm(){
+function TenantForm() {
 
     /*There are more efficient ways of handling 
     this much form input, but using curder method due to time contraints!*/
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [apartment, setApartment] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
 
-    function handleFirstName(e){
+    function handleFirstName(e) {
         e.preventDefault();
         setFirstName(e.target.value)
     }
-    function handleLastName(e){
+    function handleLastName(e) {
         e.preventDefault();
         setLastName(e.target.value)
     }
-    function handleApartment(e){
+    function handleApartment(e) {
         e.preventDefault();
         setApartment(e.target.value)
     }
+    function handlePhone(e) {
+        e.preventDefault();
+        setPhone(e.target.value)
+    }
+    function handleEmail(e) {
+        e.preventDefault();
+        setEmail(e.target.value)
+    }
 
-    async function handleSubmit(e){
+    async function handleSubmit(e) {
         e.preventDefault();
         let response = await axios.post("http://localhost:3000/tenants",
-        {first_name: firstName, last_name: lastName, apartment: apartment},
-        {headers: {Accept: "application/json"}}
+            { first_name: firstName, last_name: lastName, apartment: apartment, phone: phone, email: email },
+            { headers: { Accept: "application/json" } }
         )
         let data = response.data;
         console.log(response.data);
     }
 
-    return(
+    //form for taking input when creating a new tenant 
+    return (
         <div>
-            <label>
-                First Name:
-                <input name ="first name" value={firstName} type="Text" onChange={handleFirstName}/>
-            </label>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    First Name:
+                    <input name="first name" value={firstName} type="Text" onChange={handleFirstName} required />
+                </label>
 
-            <label>
-                Last Name:
-                <input name="second name" value={lastName} type="Text" onChange={handleLastName}/>
-            </label>
+                <label>
+                    Last Name:
+                    <input name="second name" value={lastName} type="Text" onChange={handleLastName} required />
+                </label>
 
-            <label>
-                Apartment:
-                <input name="apartment" value={apartment} type="Text" onChange={handleApartment}/>
-            </label>
+                <label>
+                    Apartment:
+                    <input name="apartment" value={apartment} type="Text" onChange={handleApartment} required />
+                </label>
 
-            <button onClick={handleSubmit}>Submit</button>
+                <label>
+                    Phone:
+                    <input name="phone" value={phone} type="Text" onChange={handlePhone} minlength="10" maxlength="10"  required />
+                </label>
+
+                <label>
+                    Email:
+                    <input name="email" value={email} type="email" onChange={handleEmail} required />
+                </label>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
